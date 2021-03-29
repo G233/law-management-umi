8a42ec2b80e9f71a3dac6b5b3fb9117e74a0b9a8
AtomG <atomg1024@gmail.com>
2021-03-29 09:03:30 +0800

    完成单项审批功能

--- src/components/Case/approvingCases.tsx
+++ src/components/Case/approvingCases.tsx
@@ -15,6 +15,8 @@ import {
 export default function approvingCases() {
   const [isShowModal, setIsShowModal] = useState(false);
   const [selectedCasesId, setSelectedCasesId] = useState<(string | number)[]>();
+  const [argBtnLoding, setArgBtnLoding] = useState(false);
+
   const { initialState } = useModel('@@initialState');
   const userInfo = initialState?.currentUser;
 
@@ -67,25 +69,45 @@ export default function approvingCases() {
     },
     {
       title: '操作',
-      width: 100,
+      width: 140,
       valueType: 'option',
       fixed: 'right',
       align: 'center',
-      render: (e) => [
-        <div key="action">
+      render: (text, record) => [
+        <div key={record._id}>
           <Row justify="center">
-            <Col span={10}>
-              <Button type="link">同意</Button>
-            </Col>
-            <Col span={10}>
-              <Button type="link" danger>
-                拒绝
+            <Col span={8}>
+              <Button
+                onClick={async () => {
+                  setArgBtnLoding(true);
+                  await oneClickApprove(
+                    [record?._id as string],
+                    ref as React.MutableRefObject<ActionType>,
+                    userInfo?.uid as string,
+                    CaseStatus.AGREE,
+                  );
+                  setArgBtnLoding(false);
+                }}
+                loading={argBtnLoding}
+                type="link"
+              >
+                同意
               </Button>
             </Col>
-          </Row>
-          <Row justify="center">
-            <Col>
-              <Button type="text">查看详情</Button>
+            <Col span={8}>
+              <Button
+                type="link"
+                danger
+                onClick={async () => {
+                  setIsShowModal(true);
+                  setSelectedCasesId([record?._id as string]);
+                }}
+              >
+                不同意
+              </Button>
+            </Col>
+            <Col span={8}>
+              <Button type="link">详情</Button>
             </Col>
           </Row>
         </div>,
